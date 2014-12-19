4f52ae216b1dd230763ef649f9615add5867d8d7
yyosifov <cypressx@gmail.com>
2014-12-19 18:09:20 +0200

    adding zabbix resource client

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var Config = require('./Config'),
 	ZabbixApi = require('./lib/zabbix-api');
 
+/**
+ * Handle an error - log it to the Output
+ * @param err
+ * @returns {*}
+ */
 var handleError = function(err) {
     if(typeof err === 'string') {
         return console.log('Error: ' + err);
@@ -10,9 +15,21 @@ var handleError = function(err) {
 };
 
 var zabbixApi = new ZabbixApi(Config.host);
+// Authenticate to the Zabbix
 zabbixApi.login(Config.user, Config.password).then(function() {
-    zabbixApi.hostgroup.create('yo').then(function(groupId) {
-        console.log('groupid = ' + JSON.stringify(groupid));
-    }, handleError);
 
+    // create a host group if it does not exist
+    zabbixApi.hostgroup.exists({
+        name: 'yo'
+    }).then(function(data) {
+        var exists = data.result;
+        if(!exists) {
+            // create new host group
+            zabbixApi.hostgroup.create({
+                name: 'yo'
+            }).then(function(groupId) {
+                console.log('Group Id = ' + JSON.stringify(groupId));
+            }, handleError);
+        }
+    }, handleError);
 }, handleError);
